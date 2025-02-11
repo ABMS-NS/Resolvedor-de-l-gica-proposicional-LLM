37d19c2af83d1217bb73fce4424769bf6202b93e
Alison Bruno <161540994+ABMS-NS@users.noreply.github.com>
2025-02-11 13:14:51 -0300

    Adição do resolvedor

---
+++ Gemini/main.py
@@ -0,0 +1,13 @@
+from gemini import Gemini
+import sys
+
+# →
+n_sentencas = input("Digite o número de sentenças que existem no seu problema: ")
+print("Digite as sentenças (pressione Ctrl+D para finalizar):\nEx:\nA: Chuva\nB: Rua Molhada\nC: Rua Escorregadia\n")
+sentencas = sys.stdin.read().strip()
+print("Digite o problema utilizando quebra de linha para separar cada sentença e o número da mesma na frente (pressione Ctrl+D para finalizar): \nEx:\n1-A → B\n2-B → C\n3-A\n")
+problema = sys.stdin.read().strip()
+
+Gemini.resolvedor(n_sentencas,sentencas,problema)
+
+
