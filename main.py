e444df0809fed838bf8fe80be64ed1a49a974542
Alison Bruno <161540994+ABMS-NS@users.noreply.github.com>
2025-02-26 15:50:55 -0300

    adição da main que interliga todas as LLMS para votação

---
+++ main.py
@@ -0,0 +1,7 @@
+from gemini import Gemini
+from qwen import Qwen 
+from deepseek import DeepSeek
+
+# nessa main será realizado o processo da votação das respostas da LLM e a contagem dos votos
+# e a exibição do resultado
+
