3c039ab7b222a4436dce0c2e7fa5b632c90e0fc4
Rian Américo <rianamericobs@outlook.com>
2025-03-09 20:45:55 -0300

    Create deepseek_comp.py

---
+++ comparadores/deepseek_comp.py
@@ -0,0 +1,20 @@
+from gemini import Gemini
+from qwen import Qwen
+from deepseek import DeepSeek
+import sys
+
+
+n_sentencas = input("Digite o número de sentenças que existem no seu problema: ")
+print("Digite as sentenças (pressione Ctrl+D para finalizar):\nEx:\nA: Chuva\nB: Rua Molhada\nC: Rua Escorregadia\n")
+sentencas = sys.stdin.read().strip()
+print("Digite o problema utilizando quebra de linha para separar cada sentença e o número da mesma na frente (pressione Ctrl+D para finalizar): \nEx:\n1-A → B\n2-B → C\n3-A\n")
+problema = sys.stdin.read().strip()
+
+resposta = DeepSeek.resolvedor(n_sentencas,sentencas,problema)
+
+avaliacao_qwen = Qwen.avaliadorbinario(resposta)
+avaliacao_gemini = Gemini.avaliadorbinario(resposta)
+
+print ("Resposta: ",resposta)
+print("Avaliação Qwen: ",avaliacao_qwen)
+print("Avaliação DeepSeek: ",avaliacao_gemini)
