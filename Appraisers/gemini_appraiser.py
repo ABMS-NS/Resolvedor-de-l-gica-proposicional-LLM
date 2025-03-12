f4915d19e730e729a4093901e9e572b04fe48286
Alison Bruno <161540994+ABMS-NS@users.noreply.github.com>
2025-03-12 16:05:02 -0300

    Criação da pasta Appraisers e da função gemini_appraiser

---
+++ Appraisers/gemini_appraiser.py
@@ -0,0 +1,18 @@
+from gemini import Gemini
+from qwen import Qwen 
+from deepseek import DeepSeek
+
+#scan the logical problem for the best
+problem = input("Enter the problem: ")
+
+#resolve the problem with Gemini
+resolution = Gemini.resolver(problem)
+
+#appraise the resolution with Qwen and DeepSeek
+appraisal_qwen = Qwen.appraiser(resolution)
+appraisal_ds = DeepSeek.appraiser(resolution)
+
+#display the resolution and appraisals
+print(f"{resolution}\n------------------------------------------------------------\n")
+print(f"Qwen: {appraisal_qwen}\n------------------------------------------------------------\n")
+print(f"DeepSeek: {appraisal_ds}\n------------------------------------------------------------\n")
